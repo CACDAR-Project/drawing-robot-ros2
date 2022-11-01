4824653d55a1b87dceff39c430af551a8a436da6
Nicolas Hiillos <nicolashiillos@gmail.com>
2022-11-01 20:19:15 +0200

    Fix destroy node

--- src/draw_svg/src/py/draw_svg.py
+++ src/draw_svg/src/py/draw_svg.py
@@ -104,7 +104,7 @@ def main(args=None):
     # Destroy the node explicitly
     # (optional - otherwise it will be done automatically
     # when the garbage collector destroys the node object)
-    minimal_publisher.destroy_node()
+    publisher.destroy_node()
     rclpy.shutdown()
 
 
