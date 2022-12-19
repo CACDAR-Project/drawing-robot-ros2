3b56bac43e9ecbdde3b06cdf6619287380eb914f
Nicolas Hiillos <nicolashiillos@gmail.com>
2022-12-19 21:00:51 +0200

    Improve motion service

--- src/drawing_controller/drawing_controller/drawing_controller.py
+++ src/drawing_controller/drawing_controller/drawing_controller.py
@@ -11,6 +11,8 @@ import math
 #from tf2_ros.transformations import quaternion_from_euler
 import lxml.etree as ET
 
+from robot_interfaces.srv import ExecuteMotion
+import sys
 
 def quaternion_from_euler(ai, aj, ak):
     ai /= 2.0
@@ -50,19 +52,24 @@ def map_point_function(x_pixels, y_pixels, xlim_lower, xlim_upper, ylim_lower, y
 
 
 class DrawingController(Node):
-    def __init__(self):
+    def __init__(self, svgpath):
         super().__init__('drawing_controller')
-        self.publisher_ = self.create_publisher(PoseStamped, '/target_pose', 10)
+        #self.publisher_ = self.create_publisher(PoseStamped, '/target_pose', 10)
         timer_period = 7.0  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 0
+
+        self.cli = self.create_client(ExecuteMotion, 'dummy_execute_path')
+        while not self.cli.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('service not available, waiting again...')
+        self.req = ExecuteMotion.Request()
 
         # TODO get dimensions from svg
 
         #print(p)
         #print(p.position)
         #print(p.orientation)
-        xml = ET.parse('svg/test.svg')
+        xml = ET.parse(svgpath)
         svg = xml.getroot()
         self.map_point = map_point_function(float(svg.get('width')), float(svg.get('height')), 0.1, 0.5, -0.2, 0.2)
         self.points = []
@@ -88,16 +95,24 @@ class DrawingController(Node):
         ps = PoseStamped()
         ps.pose = p
         #print(ps)
-        self.publisher_.publish(ps)
-        self.get_logger().info('Publishing to /target_pose: "%s"' % p)
+        #self.publisher_.publish(ps)
+        #self.get_logger().info('Publishing to /target_pose: "%s"' % p)
         self.i = (self.i + 1) % len(self.points)
+
+        #self.req.a = a
+        #self.req.b = b
+        self.future = self.cli.call_async(self.req)
+        #rclpy.spin_until_future_complete(self, self.future)
+        self.get_logger().info('Got result: "%s"' % self.future.result())
+        #return self.future.result()
+
 
 def main(args=None):
     rclpy.init(args=args)
 
-    publisher = PublishTarget()
+    dc = DrawingController(sys.argv[1])
 
-    rclpy.spin(publisher)
+    rclpy.spin(dc)
 
     # Destroy the node explicitly
     # (optional - otherwise it will be done automatically
--- src/robot_controller/src/cpp/robot_controller.cpp
+++ src/robot_controller/src/cpp/robot_controller.cpp
@@ -46,6 +46,27 @@ private:
   rclcpp::Service<robot_interfaces::srv::ExecuteMotion>::SharedPtr service;
 };
 
+// A controller for a Dummy robot. Only logs messages and serves as an example for real implementation.
+// CRTP pattern used here https://en.wikipedia.org/wiki/Curiously_recurring_template_pattern
+//class DummyController : public RobotController<DummyController>
+class DummyController : public RobotController
+{
+  public:
+
+    DummyController(std::string name) : RobotController(name) {}
+
+    void executePath(const std::shared_ptr<robot_interfaces::srv::ExecuteMotion::Request> request, std::shared_ptr<robot_interfaces::srv::ExecuteMotion::Response> response) const
+    {
+      //request->motion->path PoseStamped[]
+      //request->motion->acceleration float64
+      //request->motion->velocity float64
+      RCLCPP_INFO(this->get_logger(), "NEW MOTION: Acceleration: ");
+      //RCLCPP_INFO(this->get_logger(), "Acceleration: " + std::to_string(request.motion.acceleration));
+      response->status = "executePath not implemented";
+      RCLCPP_WARN(this->get_logger(), "AAAAAA executePath not implemented");
+    }
+
+};
 
 //RobotController::RobotController(name) : Node(name)
 
@@ -53,8 +74,8 @@ int main(int argc, char ** argv)
 {
   rclcpp::init(argc, argv);
 
-  RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Starting generic robot_controller");
-  auto robot = std::make_shared<RobotController>("generic");
+  RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Starting dummy_controller");
+  auto robot = std::make_shared<DummyController>("dummy");
 
   rclcpp::executors::SingleThreadedExecutor executor;
   executor.add_node(robot);
