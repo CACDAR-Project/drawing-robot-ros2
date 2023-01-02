d7d8177677b0465d5b609ad219df6aed9949a4a1
Nicolas Hiillos <nicolashiillos@gmail.com>
2023-01-02 12:26:13 +0200

    Fix port

--- src/axidraw_controller/src/test.py
+++ src/axidraw_controller/src/test.py
@@ -2,12 +2,13 @@
 from pyaxidraw import axidraw   # import module
 ad = axidraw.AxiDraw()          # Initialize class
 ad.interactive()                # Enter interactive context
-ad.options.port = "/dev/ttyAXI"
+ad.options.port = "/dev/ttyACM0"
 if not ad.connect():            # Open serial port to AxiDraw;
     quit()                      #   Exit, if no connection.
 ad.options.units = 1            # set working units to cm.
+ad.options.model = 2            # set model to AxiDraw V3/A3
                                 # Absolute moves follow:
-ad.moveto(1, 1)                 # Pen-up move to (1 inch, 1 inch)
-ad.lineto(2, 1)                 # Pen-down move, to (2 inch, 1 inch)
+ad.moveto(10, 10)               # Pen-up move to (10cm, 10cm)
+ad.lineto(20, 10)               # Pen-down move, to (20cm, 10)
 ad.moveto(0, 0)                 # Pen-up move, back to origin.
 ad.disconnect()                 # Close serial port to AxiDraw
