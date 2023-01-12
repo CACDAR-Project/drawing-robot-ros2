1bf28eeb87fef830a4cb90fff9aea829676052bf
Nicolas Hiillos <nicolashiillos@gmail.com>
2023-01-12 17:32:45 +0200

    Fix build

--- rebuild.sh
+++ rebuild.sh
@@ -13,7 +13,8 @@ rosdep install -y -r -i --rosdistro "humble" --from-paths src
 cd src
 rm -r install build log
 #colcon build --merge-install --symlink-install --cmake-args "-DCMAKE_BUILD_TYPE=Release" && \
-colcon build --packages-select robot_interfaces robot_controller
+#colcon build --packages-select robot_interfaces robot_controller
+colcon build --paths robot_interfaces robot_controller
 source install/local_setup.bash
 colcon build
 source install/local_setup.bash
